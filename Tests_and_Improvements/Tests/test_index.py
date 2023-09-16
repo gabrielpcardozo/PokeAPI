19547937a3f935e654bb2ec4584819782b810697
gabrielpcardozo <122734264+gabrielpcardozo@users.noreply.github.com>
2023-09-16 13:16:24 -0300

    Delete Tests_and_Improvements directory
    Organização dos diretório que são necessários somente no projeto local
    
    Vou subir novamente somente o arquivo debugger.py para caso alguém queira me dar um feedback sobre como eu realizo os meus testes.

--- Tests_and_Improvements/Tests/test_index.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.insert(0, '/home/gcardozo/Projects/Python/PokeAPI/App/Controllers')
-
-from App.Controllers import controller_pokemon
-
-pokemon = 'pikcahu'
-print(controller_pokemon.PokemonAPI().get_main_pokemon(pokemon))
